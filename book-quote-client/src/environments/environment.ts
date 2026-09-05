06dcaca1324446bc7878a50df7be6380b24533c2
Marlin  Shenoda <105081672+marlinshenoda@users.noreply.github.com>
2026-09-05 17:44:46 +0200

    Change environment to development mode

--- book-quote-client/src/environments/environment.ts
+++ book-quote-client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  apiUrl: 'https://bookquote-f8c2c7gac5dyhucg.swedencentral-01.azurewebsites.net'
+  production: false,
+  apiUrl: 'https://bookquote-f8c2c7gac5dyhucg.swedencentral-01.azurewebsites.net/api'
 };
