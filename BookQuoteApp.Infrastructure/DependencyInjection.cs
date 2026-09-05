81a07573a1f6654fb4334191c0bb07d679006966
marlinshenoda <marlinadel2000@gmail.com>
2026-09-05 17:33:56 +0200

    fix: update prodconnection with cloudsa admin username

--- BookQuoteApp.Infrastructure/DependencyInjection.cs
+++ BookQuoteApp.Infrastructure/DependencyInjection.cs
@@ -16,7 +16,7 @@ public static class DependencyInjection
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlite(configuration.GetConnectionString("ProdConnection")));
+             options.UseSqlServer(configuration.GetConnectionString("ProdConnection")));
 
         services.AddIdentity<ApplicationUser, IdentityRole>(options =>
             {
